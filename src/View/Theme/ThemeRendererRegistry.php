43692ac9f3f7dd01528b03f57abfd4acbaec14ad
Fattain Naime <iamnaime@builderhall.com>
2026-07-04 04:32:54 +0600

    refactor(theme): promote ThemeRendererRegistry to readonly property; add missing-twig fallback test (#32)

--- src/View/Theme/ThemeRendererRegistry.php
+++ src/View/Theme/ThemeRendererRegistry.php
@@ -13,13 +13,9 @@ use InvalidArgumentException;
  */
 final class ThemeRendererRegistry
 {
-    /** @var array<string, ThemeRendererInterface> */
-    private array $renderers;
-
     /** @param array<string, ThemeRendererInterface> $renderers */
-    public function __construct(array $renderers)
+    public function __construct(private readonly array $renderers)
     {
-        $this->renderers = $renderers;
     }
 
     public function get(string $engine): ThemeRendererInterface
